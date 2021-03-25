011306f19d21ca7479c7e1536f4d041a23e5fd3d
romgere <jerome_mestres@ultimatesoftware.com>
2021-03-25 15:17:32 +0100

    chore: yarn lint:js --fix

--- tests/integration/components/settings-form/settings-test.ts
+++ tests/integration/components/settings-form/settings-test.ts
@@ -1,26 +1,26 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | settings-form/settings', function(hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{settings-form/settings}}`);
+    await render(hbs`{{settings-form/settings}}`)
 
-    assert.equal(this.element.textContent?.trim(), '');
+    assert.equal(this.element.textContent?.trim(), '')
 
     // Template block usage:
     await render(hbs`
       {{#settings-form/settings}}
         template block text
       {{/settings-form/settings}}
-    `);
+    `)
 
-    assert.equal(this.element.textContent?.trim(), 'template block text');
-  });
-});
+    assert.equal(this.element.textContent?.trim(), 'template block text')
+  })
+})
--- tests/integration/components/ui/font-picker/select-test.ts
+++ tests/integration/components/ui/font-picker/select-test.ts
@@ -1,26 +1,26 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | ui/font-picker/select', function(hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{ui/font-picker/select}}`);
+    await render(hbs`{{ui/font-picker/select}}`)
 
-    assert.equal(this.element.textContent?.trim(), '');
+    assert.equal(this.element.textContent?.trim(), '')
 
     // Template block usage:
     await render(hbs`
       {{#ui/font-picker/select}}
         template block text
       {{/ui/font-picker/select}}
-    `);
+    `)
 
-    assert.equal(this.element.textContent?.trim(), 'template block text');
-  });
-});
+    assert.equal(this.element.textContent?.trim(), 'template block text')
+  })
+})
