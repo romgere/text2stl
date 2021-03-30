40b7f2a4f4b23798ba8f3c9d90c5e936070e0911
romgere <jerome_mestres@ultimatesoftware.com>
2021-03-30 17:57:35 +0200

    fix: fix type image not displayed on prod

--- app/components/ui/type-select.ts
+++ app/components/ui/type-select.ts
@@ -9,6 +9,14 @@ interface UiTypeSelectArgs {
 export default class UiTypeSelect extends Component<UiTypeSelectArgs> {
   types = Object.keys(ModelType).filter((k) => !isNaN(Number(k))).map((k) => Number(k))
 
+  // handle ember-cli finger printing
+  typeImages = {
+    1: 'img/type_1.png',
+    2: 'img/type_2.png',
+    3: 'img/type_3.png',
+    4: 'img/type_4.png'
+  }
+
   get numberValue() {
     return Number(this.args.value)
   }
