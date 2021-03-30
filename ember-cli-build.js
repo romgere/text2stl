c5bc500b022596410a2e2fc790a787a7826ae6fe
romgere <jerome@mestres.fr>
2021-03-30 23:04:54 +0200

    feat: test of prember

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    prember: {
+      urls: [
+        '/',
+        '/en-us/generate',
+        '/en-us/generate'
+      ]
+    }
   })
 
   // Use `app.import` to add additional libraries to the generated
