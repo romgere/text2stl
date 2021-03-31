20da7f1f62453491a2dc745b03cb07c61db1d347
romgere <jerome@mestres.fr>
2021-03-31 22:10:56 +0200

    feat: add settings-form integration test

--- tests/integration/components/settings-form-test.ts
+++ tests/integration/components/settings-form-test.ts
@@ -1,26 +1,47 @@
 import { module, test } from 'qunit'
 import { setupRenderingTest } from 'ember-qunit'
-import { render } from '@ember/test-helpers'
+import { render, click } from '@ember/test-helpers'
 import hbs from 'htmlbars-inline-precompile'
+import Component from '@ember/component'
 
 module('Integration | Component | settings-form', function(hooks) {
   setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{settings-form}}`)
+    this.owner.register(
+      'component:settings-form/settings',
+      class extends Component {
+        layout = hbs`<div data-mocked-settings data-model={{@model}} />`
+      }
+    )
 
-    assert.equal(this.element.textContent?.trim(), '')
+    this.owner.register(
+      'component:settings-form/font',
+      class extends Component {
+        layout = hbs`<div data-mocked-font data-model={{@model}} data-on-font-change={{@onFontChange}} />`
+      }
+    )
 
-    // Template block usage:
-    await render(hbs`
-      {{#settings-form}}
-        template block text
-      {{/settings-form}}
-    `)
+    await render(hbs`<SettingsForm @model="the_model" @onFontChange="on_font_change" />`)
 
-    assert.equal(this.element.textContent?.trim(), 'template block text')
+    assert.dom('[uk-tab]').exists('It render tab')
+    assert.dom('[uk-tab] li').exists({ count: 2 }, 'It render 2 tabs')
+
+    assert.dom('[data-mocked-font]').isNotVisible('Font tab is not visible')
+    assert
+      .dom('[data-mocked-settings]')
+      .exists('It render settings tab by default')
+      .hasAttribute('data-model', 'the_model', 'Model is passed to settings component')
+
+    // Change tab
+    await click('[data-tab-item=font] a')
+
+    assert.dom('[data-mocked-settings]').isNotVisible('Settings tab is not visible')
+    assert
+      .dom('[data-mocked-font]')
+      .exists('font tab is rendered')
+      .hasAttribute('data-model', 'the_model', 'Model is passed to font component')
+      .hasAttribute('data-on-font-change', 'on_font_change', 'Model is passed to font component')
   })
 })
