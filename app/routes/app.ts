7a1b981637467f8c553c36b076790c0a7dc79767
romgere <jerome_mestres@ultimatesoftware.com>
2021-04-01 18:06:14 +0200

    feat: add app loader

--- app/routes/app.ts
+++ app/routes/app.ts
@@ -9,4 +9,8 @@ export default class AppRoute extends Route {
   async model({ locale }: { locale: string }) {
     this.intl.locale = locale === 'en-us' ? locale : [locale, 'en-us']
   }
+
+  afterModel() {
+    document.querySelector('#app-loader')?.remove()
+  }
 }
