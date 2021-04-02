b478dc2011a7834aa5e772b3b773a308b8cfd5f4
romgere <jerome_mestres@ultimatesoftware.com>
2021-04-02 11:05:36 +0200

    feat: replace static image with mesh slide on homepage

---
+++ app/routes/app/index.ts
@@ -0,0 +1,26 @@
+import Route from '@ember/routing/route'
+import { THREE } from 'enable3d'
+
+let loader : THREE.ObjectLoader | undefined = undefined
+
+function loadJSONMesh(path: string) :Promise<THREE.Object3D> {
+
+  if (!loader) {
+    loader = new THREE.ObjectLoader()
+  }
+
+  return new Promise(function(resolve, reject) {
+    loader?.load(path, resolve, () => {}, reject)
+  })
+}
+
+export default class AppIndex extends Route {
+  async model() {
+    return [
+      await loadJSONMesh('/mesh/1.json'),
+      await loadJSONMesh('/mesh/2.json'),
+      await loadJSONMesh('/mesh/3.json'),
+      await loadJSONMesh('/mesh/4.json')
+    ]
+  }
+}
