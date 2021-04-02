9eb4e7b0663cd2c5eff554c4500e81325b75affd
romgere <jerome_mestres@ultimatesoftware.com>
2021-04-02 11:03:43 +0200

    feat: add a plus helper

---
+++ app/helpers/plus.ts
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper'
+
+export function plus([value, add = 1]: [number, number]) {
+  return Number(value) + Number(add)
+}
+
+export default helper(plus)
