9213b6d4d978f09a8e160703268302422345ee8c
Jérôme Mestres <jerome@mestres.fr>
2023-08-09 13:03:32 +0200

    update ember types

--- types/qunit-parameterize.d.ts
+++ types/qunit-parameterize.d.ts
@@ -1,6 +1,6 @@
 declare module 'qunit-parameterize' {
 
-  import { TestContext } from 'ember-test-helpers'
+  import { type TestContext } from '@ember/test-helpers';
 
   interface CasesFunction<T> {
     test: (name: string, callback: (this: TestContext, attrs : T, assert: Assert) => void | Promise<void>) => void;
