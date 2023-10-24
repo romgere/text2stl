cee787887b359e920c18196f49a30fbbd2f979e0
Jérôme Mestres <jerome@mestres.fr>
2023-10-24 14:25:05 +0200

    remove old gtag code

--- app/initializers/gtag.ts
+++ app/initializers/gtag.ts
@@ -2,7 +2,9 @@ import config from 'text2stl/config/environment';
 const { gTag, environment } = config;
 
 export function initialize() {
-  if (!gTag.forceEnable && ['development', 'test'].includes(environment)) {
+  if (!gTag?.forceEnable && ['development', 'test'].includes(environment)) {
+    return;
+  } else if (!gTag?.tag) {
     return;
   }
 
@@ -12,7 +14,7 @@ export function initialize() {
 
 async function injectGtag() {
   const script = document.createElement('script');
-  script.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${gTag.tag}`);
+  script.setAttribute('src', `https://www.googletagmanager.com/gtag/js?id=${gTag?.tag}`);
   document.body.appendChild(script);
 }
 
@@ -22,7 +24,7 @@ function initGtag() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '${gTag.tag}');
+    gtag('config', '${gTag?.tag}');
 `;
 
   // Append the script tag to the document.
